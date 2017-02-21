89e51c47f38c210db04de5fcda4b9bbd04213cce
Bill Ferster <bferster@virginia.edu>
2017-02-21 17:51:38 -0500

    favison

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ class App  {
         this.curLesson=this.curTopic=this.curConcept=this.curStep=0;
         this.name="WriteMao";
         this.doc=new Doc();
-        this.content=new Content();
+        this.con=new Content();
+        this.nav=new Navigation();
         this.Draw(); 
      }
      
      Draw() {
-        this.content.Draw();
+        this.con.Draw();
+        this.nav.Draw();
      }
 }
---
+++ navigation.js
@@ -0,0 +1,13 @@
+class Navigation {
+    constructor() {
+
+    }
+    Forward(){
+    }
+    Back() {
+    }
+    Draw() {
+trace(123)   
+
+ }
+}
