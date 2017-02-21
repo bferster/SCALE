a3d3508069e41d6d741270d69f07d10cf2f42c46
Bill Ferster <bferster@virginia.edu>
2017-02-21 16:40:56 -0500

    debug

--- doc.js
+++ doc.js
@@ -21,38 +21,3 @@ class Doc  {
         this.lastName="Bruner";
         }
   }
-
-
-class Lesson  {
-    constructor(o)   {
-    this.topics=[];
-    }
-}
-
-class Topic  {
-    constructor(o)   {
-    this.concepts=[];
-    }
-}
-
-class Concept  {
-    constructor(o)   {
-    this.steps=[];
-    }
-}
-
-class Page  {
-    constructor(o)   {
-    this.header;
-    }
-}
-
-class Step  {
-    constructor(o)   {
-        this.type=o.type == undefined ? "tell" : o.type;
-        this.title=!o.title ? "" : o.title;
-        
-    }
-}
-
-
