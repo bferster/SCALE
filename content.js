d7882c43ec360ec66e0413f7ed8875ebcd6f6509
Bill Ferster <bferster@virginia.edu>
2018-07-11 16:53:15 -0400

    margina

--- content.js
+++ content.js
@@ -34,9 +34,9 @@ class Content  {
 		str+="<div id='contentBodyDiv' class='wm-contentBody'>";													// Container div
 		if (l) {																									// Valid lob
 			str+=l.body ? l.body : "";																				// Add body
-			if (ifr=str.match(/scalemedia\((.+)\)/i)) {																// If a media tag
+			if (ifr=str.match(/scalemedia\((.*?)\)/i)) {															// If a media tag
 				var w=99.5,b=0;																						// Assume full width
-				var h=$("#contentDiv").height()-180;																// Set default height												
+				var h=$("#contentDiv").height()-200;																// Set default height												
 				ifr=(""+ifr[1]).split(",");																			// Get params
 				if (ifr[1])		h=h*ifr[1]/100;																		// Calc height based on height percentage
 				if (ifr[2])		w=w*ifr[2]/100;																		// Width too
@@ -45,26 +45,26 @@ class Content  {
 					ifr[0]+="|start="+this.resumeTime;																// Set new start
 				ifs="<div style='text-align:center'>";																// For centering
 				ifs+="<iframe id='contentIF' class='wm-media' align='middle' frameborder='"+b+"' src='"+ifr[0]+"' style='height:"+h+"px;width:"+w+"%'></iframe></div>";	// Load in iframe
-				str=str.replace(/scalemedia\(.+\)/i,ifs);															// Get tag and replace it with iframe
+				str=str.replace(/scalemedia\(.*?\)/i,ifs);															// Get tag and replace it with iframe
 				}
-			else if (ifr=str.match(/assess\((.+)\)/i)) {															// If an assess tag
+			if (ifr=str.match(/assess\((.*?)\)/i)) {																// If an assess tag
 				var w=99.5,b=0;																						// Assume full width
 				var h=$("#contentDiv").height()-250;																// Set default height												
 				ifs="<div style='text-align:center'>";																// For centering
 				var url="assess.htm?"+ifr[1];
 				url+="|"+l.id;																						// Add overall assessment id
 				ifs+="<iframe id='contentIF' class='wm-media' align='middle' frameborder='0' src='"+url+"' style='height:"+h+"px;width:66%'></iframe></div>";	// Load in iframe
-				str=str.replace(/assess\(.+\)/i,ifs);																// Get tag and replace it with iframe
+				str=str.replace(/assess\(.*?\)/i,ifs);																// Get tag and replace it with iframe
 				}
-			else if (ifr=str.match(/margin\((.+)\)/i)) {															// If a margin tag
+			if (ifr=str.match(/margin\((.*?)\)/i)) {																// If a margin tag
 				margin=ifr[1];																						// Set margin
-				str=str.replace(/margin\(.+\)/i,"");																// Kill tag
+				str=str.replace(/margin\(.*?\)/i,"");																// Kill tag
 				}
 			$("#contentDiv").append(str+"</div>");																	// Set content
 	
-			if (margin)	{																							// If a margin set
+			if (margin && (margin != "0"))	{																		// If a margin set
 				var pct=100-margin-margin;																			// Width
-				margin+="%";																						// Add %
+				margin+="%";																					// Add %
 				$("#contentBodyDiv").css({ "padding-left":margin,"padding-right":margin,width:pct+"%" });			// Set margin	
 				}	
 		}
