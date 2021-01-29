22341ed021110071543f8e93486ca90a855e5f0f
bferster <bferster@stagetools.com>
2021-01-29 18:32:16 -0500

    pane title macro

--- content.js
+++ content.js
@@ -146,6 +146,12 @@ class Content  {
 					str=str.replace(/do\(.+?\)/i,d);																// Replace tag
 					}
 				}
+			if ((ifr=str.match(/panetitle\(.+?\)/ig))) {															// If a panetitle() tag
+				str=str.replace(/panetitle\(.+?\)/i,"");															// Hide tag
+				let r=new RegExp("wm-pageTitle'>"+l.name);															// Create regex
+				ifr[0]=(""+ifr[0]).substring(10,ifr[0].length-1);													// Carve out new title
+				str=str.replace(r,"wm-pageTitle'>"+ifr[0]);															// Replace title
+				}
 			if ((ifr=str.match(/textbox\(.+?\)/ig))) {																// If a textbox() tag
 				var i,t;	
 				for (i=0;i<ifr.length;++i) {																		// For each do() macro
@@ -214,7 +220,9 @@ class Content  {
 */
 			$("#contentDiv").append(str+"</div>");																	// Set content
 			$("#contentDiv").css("max-width",app.fullScreen ? "calc(100% - 16px)" : "950px" )						// Reset width
-		
+
+			
+
 			if (widgetSrc) {																						// If a widget
 				widgetSrc=widgetSrc.replace(/&amp;/g,"&");															// Change &amp; -> &
 				if (!$("#contentIFWidget").length) {																// If not made yet
